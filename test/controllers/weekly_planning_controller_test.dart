d858828a93fc9db42965a4d316d98f1b9d920343
Rul1an <roelschuurkes@gmail.com>
2025-07-27 20:00:06 +0200

    test: convert WeeklyPlanningController tests to widget tests

--- test/controllers/weekly_planning_controller_test.dart
+++ test/controllers/weekly_planning_controller_test.dart
@@ -1,20 +1,49 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:jo17_tactical_manager/controllers/weekly_planning_controller.dart';
 
 void main() {
-  group('WeeklyPlanningController', () {
+  group('WeeklyPlanningController (widget)', () {
     late WeeklyPlanningController controller;
 
     setUp(() {
       controller = WeeklyPlanningController();
     });
 
-    test('initial scrollController offset is zero', () {
+    testWidgets('initial scrollController offset is zero', (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: ListView(
+            controller: controller.scrollController,
+            children: List.generate(
+              5,
+              (i) => SizedBox(height: 100),
+            ),
+          ),
+        ),
+      );
+
       expect(controller.scrollController.offset, equals(0.0));
     });
 
-    test('scrollToWeek does not throw for valid index', () {
+    testWidgets('scrollToWeek does not throw for valid index', (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: ListView(
+            controller: controller.scrollController,
+            children: List.generate(
+              10,
+              (i) => SizedBox(height: 100),
+            ),
+          ),
+        ),
+      );
+
+      // Should animate without throwing
       expect(() => controller.scrollToWeek(2), returnsNormally);
+
+      // Allow animation to complete
+      await tester.pumpAndSettle();
     });
 
     tearDown(() {
