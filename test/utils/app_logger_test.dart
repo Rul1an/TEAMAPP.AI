b3c2102a2e5a4336b8b1187b77cdb6d7da27bd3c
Rul1an <roelschuurkes@gmail.com>
2025-08-11 16:14:14 +0200

    test: initialize TestWidgetsFlutterBinding in app_logger_test to avoid channel setup issues in CI

--- test/utils/app_logger_test.dart
+++ test/utils/app_logger_test.dart
@@ -5,7 +5,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:jo17_tactical_manager/utils/app_logger.dart';
 
 void main() {
-  test('AppLogger does not throw on various levels', () {
+  test('AppLogger does not throw on various levels', () async {
+    // Ensure test binding is initialized for method channels/sentry breadcrumb calls
+    TestWidgetsFlutterBinding.ensureInitialized();
     expect(() => AppLogger.t('trace'), returnsNormally);
     expect(() => AppLogger.d('debug'), returnsNormally);
     expect(() => AppLogger.i('info'), returnsNormally);
