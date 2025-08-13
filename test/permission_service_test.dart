28e9e6d22e47dcb721539008d043c68345683951
Rul1an <roelschuurkes@gmail.com>
2025-08-13 16:31:50 +0200

    rbac: roll out Roles constants to tests and demo_mode_provider

--- test/permission_service_test.dart
+++ test/permission_service_test.dart
@@ -1,29 +1,31 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:jo17_tactical_manager/services/permission_service.dart';
+import 'package:jo17_tactical_manager/constants/roles.dart';
 import 'package:jo17_tactical_manager/models/organization.dart';
 
 void main() {
   group('PermissionService', () {
     test('identifies player & parent as view-only users', () {
-      expect(PermissionService.isViewOnlyUser('speler'), isTrue);
-      expect(PermissionService.isViewOnlyUser('ouder'), isTrue);
-      expect(PermissionService.isViewOnlyUser('hoofdcoach'), isFalse);
+      expect(PermissionService.isViewOnlyUser(Roles.speler), isTrue);
+      expect(PermissionService.isViewOnlyUser(Roles.ouder), isTrue);
+      expect(PermissionService.isViewOnlyUser(Roles.hoofdcoach), isFalse);
     });
 
     test('canManagePlayers for roles', () {
-      expect(PermissionService.canManagePlayers('bestuurder'), isTrue);
-      expect(PermissionService.canManagePlayers('hoofdcoach'), isTrue);
-      expect(PermissionService.canManagePlayers('admin'), isTrue);
-      expect(PermissionService.canManagePlayers('speler'), isFalse);
+      expect(PermissionService.canManagePlayers(Roles.bestuurder), isTrue);
+      expect(PermissionService.canManagePlayers(Roles.hoofdcoach), isTrue);
+      expect(PermissionService.canManagePlayers(Roles.admin), isTrue);
+      expect(PermissionService.canManagePlayers(Roles.speler), isFalse);
     });
 
     test('access SVS depends on tier and role', () {
       expect(
-        PermissionService.canAccessSVS('hoofdcoach', OrganizationTier.basic),
+        PermissionService.canAccessSVS(
+            Roles.hoofdcoach, OrganizationTier.basic),
         isFalse,
       );
       expect(
-        PermissionService.canAccessSVS('hoofdcoach', OrganizationTier.pro),
+        PermissionService.canAccessSVS(Roles.hoofdcoach, OrganizationTier.pro),
         isTrue,
       );
     });
