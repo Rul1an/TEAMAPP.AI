fd57ded3e9307147d6bf3d778d6242b5c508db0e
Rul1an <roelschuurkes@gmail.com>
2025-08-16 18:05:17 +0200

    analytics/settings: persist HeatMapSettings to Hive with explicit generics; analyzer clean

--- lib/providers/heat_map_settings_provider.dart
+++ lib/providers/heat_map_settings_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
 
 import '../widgets/analytics/heat_map_palette.dart';
 
@@ -38,24 +39,73 @@ class HeatMapSettings {
 }
 
 class HeatMapSettingsNotifier extends StateNotifier<HeatMapSettings> {
-  HeatMapSettingsNotifier() : super(const HeatMapSettings());
+  HeatMapSettingsNotifier() : super(const HeatMapSettings()) {
+    _load();
+  }
 
   void setPalette(HeatMapPalette palette) {
     state = state.copyWith(palette: palette);
+    _persist();
   }
 
   void setMinCount(int minCount) {
     final int safe = minCount < 1 ? 1 : minCount;
     state = state.copyWith(minCount: safe);
+    _persist();
   }
 
   void setDpEnabled({required bool enabled}) {
     state = state.copyWithAll(dpEnabled: enabled);
+    _persist();
   }
 
   void setEpsilon(double epsilon) {
     final double safe = epsilon <= 0 ? 1.0 : epsilon;
     state = state.copyWithAll(epsilon: safe);
+    _persist();
+  }
+
+  static const String _boxName = 'app_settings';
+  static const String _key = 'heatmap_settings';
+
+  Future<void> _load() async {
+    try {
+      final Box<Map<dynamic, dynamic>> box =
+          await Hive.openBox<Map<dynamic, dynamic>>(_boxName);
+      final Map<dynamic, dynamic>? data = box.get(_key);
+      if (data == null) return;
+      final paletteIndex = data['palette'] as int?;
+      final minCount = data['minCount'] as int?;
+      final dpEnabled = data['dpEnabled'] as bool?;
+      final epsilon = (data['epsilon'] as num?)?.toDouble();
+      state = state.copyWithAll(
+        palette: paletteIndex != null &&
+                paletteIndex >= 0 &&
+                paletteIndex < HeatMapPalette.values.length
+            ? HeatMapPalette.values[paletteIndex]
+            : state.palette,
+        minCount: minCount ?? state.minCount,
+        dpEnabled: dpEnabled ?? state.dpEnabled,
+        epsilon: epsilon ?? state.epsilon,
+      );
+    } catch (_) {
+      // Ignore persistence errors
+    }
+  }
+
+  Future<void> _persist() async {
+    try {
+      final Box<Map<dynamic, dynamic>> box =
+          await Hive.openBox<Map<dynamic, dynamic>>(_boxName);
+      await box.put(_key, <String, dynamic>{
+        'palette': state.palette.index,
+        'minCount': state.minCount,
+        'dpEnabled': state.dpEnabled,
+        'epsilon': state.epsilon,
+      });
+    } catch (_) {
+      // Ignore persistence errors
+    }
   }
 }
 
