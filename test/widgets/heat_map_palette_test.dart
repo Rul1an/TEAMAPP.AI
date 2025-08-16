4112911bb112abfb65abe34bb4c2b4f01dbad2a1
Rul1an <roelschuurkes@gmail.com>
2025-08-16 17:02:38 +0200

    tests: add HeatMapPalette tests; analyzer clean

---
+++ test/widgets/heat_map_palette_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:jo17_tactical_manager/widgets/analytics/heat_map_palette.dart';
+
+void main() {
+  test('classic palette returns gradient from yellow to red', () {
+    final c0 = HeatMapPalette.classic.colorFor(0.0);
+    final c1 = HeatMapPalette.classic.colorFor(1.0);
+    expect(c0, isNotNull);
+    expect(c1, isNotNull);
+    expect(c0.value, isNot(equals(c1.value)));
+  });
+
+  test('viridis palette returns non-null colors across range', () {
+    for (final t in [0.0, 0.25, 0.5, 0.75, 1.0]) {
+      final c = HeatMapPalette.viridis.colorFor(t);
+      expect(c, isNotNull);
+    }
+  });
+}
